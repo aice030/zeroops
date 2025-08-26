561c2f9f4dd2529a3bd04811324977b616b75375
aice030 <cheng000811@gmail.com>
2025-08-26 14:19:04 +0800

    实现elasticsearch数据获取mcp，包含三个tool

---
+++ mcp-server/elasticsearch/internal/servers.go
@@ -0,0 +1,27 @@
+package internal
+
+import (
+	"fmt"
+	"github.com/mark3labs/mcp-go/server"
+	"log"
+	"qiniu1024-mcp-server/pkg/common/config"
+)
+
+func StartElasticsearchMcpServer() {
+	mcpServer := server.NewMCPServer(
+		"ElasticSearch MCP Service",
+		"1.0.0")
+
+	// 添加工具
+	mcpServer.AddTool(GetServiceByHostIDTool(), GetServiceByHostIDHandler)
+	mcpServer.AddTool(FetchLogsByServiceAndHostTool(), FetchLogsByServiceAndHostHandler)
+	mcpServer.AddTool(FetchRequestTraceTool(), FetchRequestTraceHandler)
+
+	// 从配置文件读取mcp-server运行的端口号和endpoint 路径
+	port := config.GlobalConfig.ElasticSearch.Port
+	endpoint := config.GlobalConfig.ElasticSearch.Endpoint
+	httpServer := server.NewStreamableHTTPServer(mcpServer, server.WithEndpointPath(endpoint))
+	addr := fmt.Sprintf(":%d", port)
+	fmt.Printf("ElasticSearch MCP Service启动于 %s%s ...\n", addr, endpoint)
+	log.Fatal(httpServer.Start(addr))
+}
