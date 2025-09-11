eda29d38fda7da6fbfeec3e5ead5bd47c156ce88
aice030 <cheng000811@gmail.com>
2025-09-11 20:11:53 +0800

    实现告警记录界面

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import ChangeLogView from '../views/ChangeLogView.vue'
+import AlertsView from '../views/AlertsView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +15,11 @@ const router = createRouter({
       path: '/changelog',
       name: 'changelog',
       component: ChangeLogView,
+    },
+    {
+      path: '/alerts',
+      name: 'alerts',
+      component: AlertsView,
     },
   ],
 })
